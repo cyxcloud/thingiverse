7a5f5dad42a05c080b8835368cc3700c7bae9cb8
cyxcloud <cyxcloud@cyxcloud.onmicrosoft.com>
2023-05-03 12:01:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'xchina.co'
+      url.hostname = 'www.thingiverse.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
